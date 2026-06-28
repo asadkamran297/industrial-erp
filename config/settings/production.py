246a8936eb82d649bc3db8634f32123e61d0bc1c
Asad Kamran <asadkamran297@yahoo.com>
2026-06-28 16:47:52 +0500

    Add CSRF_TRUSTED_ORIGINS for Railway

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,7 @@ from .base import *  # noqa: F403
 DEBUG = False
 
 ALLOWED_HOSTS = [".railway.app", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
